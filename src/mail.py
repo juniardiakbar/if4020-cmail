0b097b31e91a4d582839672c52dc0130e02accf6
Juniardi Akbar <juniardiakbar@gmail.com>
2020-11-30 23:40:08 +0700

    Indox done

--- src/mail.py
+++ src/mail.py
@@ -1,24 +1,81 @@
+import codecs
+import email
 import smtplib
+import imaplib
 import os
 from dotenv import load_dotenv
 
-def send(to, subject, message):
-    load_dotenv()
-    email = os.getenv("EMAIL")
-    password = os.getenv("PASSWORD")
+def get_mpart(mail):
+    maintype = mail.get_content_maintype()
+    if maintype == 'multipart':
+        for part in mail.get_payload():
+            if part.get_content_maintype() == 'text':
+                return part.get_payload()
+        return ""
+    elif maintype == 'text':
+        return mail.get_payload()
 
-    print(email, password)
-    
-    try:
-        mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+def get_mail_body(mail):
+    if mail.is_multipart():
+        body = get_mpart(mail)
+    else:
+        body = mail.get_payload()
+    return body
+
+class Mail:
+    def __init__(self):
+        load_dotenv()
+        self.email = os.getenv("EMAIL")
+        self.password = os.getenv("PASSWORD")
+        self.smtp = "smtp.gmail.com"
+        self.smtp_port = 587
+        self.imap = "imap.gmail.com"
+        self.imap_port = 993
+
+    def send(self, to, subject, message):
+        mail = smtplib.SMTP(self.smtp, 587)
         mail.ehlo()
         mail.starttls()
-        mail.login(email,password)
-        sub = "Subject: " + subject + "\n\n" + message
-        mail.sendmail(email,to,sub.encode('utf-8'))
+        mail.login(self.email, self.password)
+        
+        data = "Subject: " + subject + "\n\n" + message
+        mail.sendmail(self.email, to, data.encode('utf-8'))
         mail.quit()
-        print("success")
+        
         return True
-    except Exception as e:
-        print("unsuccess" + e)
-        return False
+
+    def inbox(self, page=1, limit=10):
+        output = []
+        
+        mail = imaplib.IMAP4_SSL(self.imap)
+        mail.login(self.email, self.password)
+        mail.select('inbox')
+
+        status, data = mail.search(None, 'ALL')
+        mail_ids = data[0]
+        id_list = mail_ids.split()
+
+        i = (page - 1) * limit
+        count = 0
+        while (i < len(id_list) and count < limit):
+            status, data = mail.fetch(str.encode(str(int(id_list[i]))), '(RFC822)')
+            
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_string(codecs.decode(response_part[1],'utf-8'))
+                    email_subject = msg['subject']
+                    email_body = get_mail_body(msg)
+                    email_from = msg['from']
+
+                    obj = {}
+                    obj["from"] = email_from
+                    obj["subject"] = email_subject
+                    obj["body"] = email_body
+
+                    output.append(obj)
+
+            i += 1
+            count += 1
+
+        return output
--- src/route.py
+++ src/route.py
@@ -4,6 +4,8 @@ import mail
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+m = mail.Mail()
+
 @app.route('/send', methods=['POST'])
 def send():
     body = request.json
@@ -12,7 +14,7 @@ def send():
     message = body['message']
     
     try:
-        if mail.send(to, subject, message):
+        if m.send(to, subject, message):
             return jsonify({"status": "200", "message": "Success sent email"})
         else:
             return jsonify({"status": "500", "message": "Fail sent email"})
@@ -22,6 +24,18 @@ def send():
         print(e)
         return jsonify({"status": "500", "message": "Error occured when sent email"})
 
+@app.route('/inbox', methods=['GET'])
+def inbox():
+    page = request.args.get('page')
+    if (page is None): page = 1
+    try:
+        data = m.inbox(page)
+        return jsonify({"status": "200", "message": "Success get inbox", "data": data})
+
+    except Exception as e:
+        print("ERROR WHILE SEND MAIL")
+        print(e)
+        return jsonify({"status": "500", "message": "Error occured when get inbox"})
 
 if __name__ == '__main__':
     app.run(debug=True)
