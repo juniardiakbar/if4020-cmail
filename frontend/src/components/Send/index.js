7086286c05f2a6e3aab1a68792e3614f915e60d5
Asif Hummam Rais <hashshura@gmail.com>
2020-12-01 22:28:35 +0700

    Add encryptMode and encryptKey

--- frontend/src/components/Send/index.js
+++ frontend/src/components/Send/index.js
@@ -1,22 +1,86 @@
+import { useState } from "react";
+import swal from "sweetalert";
+
 import "./styles.scss";
 
 function Send() {
+  const [to, setTo] = useState("");
+  const [subject, setSubject] = useState("");
+  const [content, setContent] = useState("");
+  const [encryptKey, setEncryptKey] = useState("");
+  const [encryptMode, setEncryptMode] = useState("");
+
+  const sendEmail = () => {
+    fetch("http://127.0.0.1:5000/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to,
+        subject,
+        message: content,
+        encryptKey,
+        encryptMode,
+      }),
+    })
+      .then((response) => response.json())
+      .then(({ status, message }) => {
+        if (status === "200") {
+          setTo("");
+          setSubject("");
+          setContent("");
+        }
+        swal(message);
+      });
+  };
+
   return (
     <form>
       <div className="title">Send Email</div>
       <div>
         <div className="label">to</div>
-        <input className="field" placeholder="Email content..." />
+        <input
+          value={to}
+          onChange={(e) => setTo(e.target.value)}
+          className="field"
+          placeholder="Destination..."
+        />
       </div>
       <div>
         <div className="label">subject</div>
-        <input className="field" placeholder="Email content..." />
+        <input
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+          className="field"
+          placeholder="Subject..."
+        />
       </div>
       <div>
         <div className="label">content</div>
-        <textarea className="field" rows="5" placeholder="Email content..." />
+        <textarea
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="field"
+          rows="5"
+          placeholder="Email content..."
+        />
       </div>
-      <input type="submit" value="Submit" />
+      <select onChange={(e) => setEncryptMode(e.target.value)}>
+        <option value="">-- encrypt mode --</option>
+        <option value="ecb">ECB</option>
+        <option value="cbc">CBC</option>
+        <option value="counter">Counter</option>
+      </select>
+      <input
+        value={to}
+        onChange={(e) => setEncryptKey(e.target.value)}
+        placeholder="Encrypt key..."
+        disabled={!encryptMode}
+      />
+      <button type="button" onClick={sendEmail}>
+        Submit
+      </button>
     </form>
   );
 }
